b792de04b55330be69affd0135293cff08a07896
xinfeng.xu <xinfeng.xu@qunar.com>
2016-09-03 23:22:18 +0800

    添加所有的项目

---
+++ SSH-SVM/src/main/java/com/feng/fresh/sampleword/TriggerLarger.java
@@ -0,0 +1,11 @@
+package com.feng.fresh.sampleword;
+
+import java.util.Map;
+
+/**
+ * Created by feng on 2016/9/1.
+ */
+public class TriggerLarger {
+
+    //public static Map<String, Integer>
+}
